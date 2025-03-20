1a1b3cf8d64218cdfc18ba7976c7175ec67ee6fe
Book Buddy <bookbuddy2025@outlook.com>
2025-03-20 15:08:09 +0530

    add models

---
+++ BookStore-Backend/models/order.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const order = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "user",
+    },
+    book: {
+        type: mongoose.Types.ObjectId,
+        ref: "books",
+    },
+    status: {
+        type: String,
+        default: "Order Placed",
+        enum: ["Order Placed", "Out for delivery", "Delivered", "Order Canceled"]
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("order", order);
